988ef3069a6d21b8295171594e1aef4d20dee191
kowalskiw <kowalskiwojciech986@gmail.com>
2017-05-12 10:27:03 +0200

    Add files via upload
    ćwiczenie z pętli, sprawdza jaki bigram powtarza się najczęściej i ile razy.

---
+++ Wojtek/petle2.py
@@ -0,0 +1,16 @@
+word = input('insert word: ')
+a = 1
+parts = []
+m = 0
+j = 0
+
+for i in range(len(word) - 1):
+    parts.append(word[i] + word[i+1])
+    if parts.count(parts[j]) > a:
+        a += 1
+        m = parts[j]
+    j += 1
+
+if a == 1:
+    amount = 0
+print('bigram "{}" recurs {} times'.format(m, a))
